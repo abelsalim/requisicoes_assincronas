fe3db8e3662c78630402d3ae315d7335cb567f2b
abelsalim <abelsalim@proton.me>
2024-02-19 20:13:08 -0300

    [ADD] utilizando limitador da classe

--- requisicoes_assincronas/executa_limitando.py
+++ requisicoes_assincronas/executa_limitando.py
@@ -1,34 +1,28 @@
 import asyncio
 
+from asyncio import create_task
 
-async def limitador(lista, limite):
-    semaphore = asyncio.Semaphore(limite)
+from models.requisicao import RequisicaoAsync
 
-    async def interno(item):
-        async with semaphore:
-            print(f'Semaphore Value: {semaphore._value}')
-            await asyncio.sleep(1)
-            print(f'Item Lista: {item}')
 
-            return item
-
-    lista_retorno = []
-
-    for indice in range(0, len(lista), limite):
-        faixa = lista[indice: indice + limite]
-
-        # Usando asyncio.gather para paralelizar as chamadas a interno
-        lista_retorno.extend(
-            await asyncio.gather(*[interno(item) for item in faixa])
-        )
-
-    return lista_retorno
+async def mostra_numero(numero):
+    return numero
 
 
 async def main():
-    retorno = [item for item in await limitador([1, 2, 4, 5, 6, 7, 8, 9], 2)]
+    async with RequisicaoAsync(limit_connector=100) as request:
+        lista_numeros = [
+            create_task(mostra_numero(numero)) for numero in range(1, 10)
+        ]
 
-    print(retorno)
+        retorno = [
+            item for item in await request.limitador(
+                tasks=lista_numeros,
+                limit=2
+            )
+        ]
+
+        print(retorno)
 
 
 asyncio.run(main())
